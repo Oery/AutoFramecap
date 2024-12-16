a82c15b55e29d16d844ea59f6e27ae7d9dd87168
Oery <oery.dev@gmail.com>
2024-12-16 11:08:54 +0100

    Add Customizable Framecap

--- AutoFramecap/ModClass.cs
+++ AutoFramecap/ModClass.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace AutoFramecap
 {
-    public class AutoFramecap : Mod, ITogglableMod
+    public class AutoFramecap : Mod, IMenuMod, ITogglableMod
     {
         internal static AutoFramecap Instance;
 
@@ -24,6 +24,10 @@ namespace AutoFramecap
             "GG_Hollow_Knight",
         ];
 
+        private int maxFramerate = 400;
+
+        public bool ToggleButtonInsideMenu => false;
+
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             Instance = this;
@@ -40,18 +44,44 @@ namespace AutoFramecap
 
         private string OnSceneLoad(string name) 
         {
-            if (cappedScenes.Contains(name))
-            {
-                Log("Capping framerate for scene " + name);
-                Application.targetFrameRate = 400;
-            }
-            else 
-            {
-                Log("Uncapping framerate for scene " + name);
-                Application.targetFrameRate = -1;
-            }
-
+            CheckScene(name);
             return name;
         } 
+
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            return
+            [
+                new() 
+                {
+                    Name = "Capped Framerate",
+                    Description = "Max FPS when capped, Game Default: 400",
+                    Values = [ "400", "300", "200", "100", "60", "30" ],
+                    Saver = idx => {
+                        maxFramerate = idx switch
+                            { 0 => 400, 1 => 300, 2 => 200, 3 => 100, 4 => 60, 5 => 30, _ => maxFramerate
+                        };
+
+                        CheckScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+                    },
+                    Loader = () => maxFramerate switch
+                        {  400 => 0, 300 => 1, 200 => 2, 100 => 3, 60 => 4, 30 => 5, _ => 0, },
+                }
+            ];
+        }
+
+        private void CheckScene(string sceneName)
+        {
+            if (cappedScenes.Contains(sceneName))
+            {
+                Log("Capping framerate for scene " + sceneName);
+                Application.targetFrameRate = maxFramerate;
+            }
+            else
+            {
+                Log("Uncapping framerate for scene " + sceneName);
+                Application.targetFrameRate = -1;
+            }
+        }        
     }
 }
